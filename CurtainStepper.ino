f7a341448340526108d4514290076a8a835ae0ac
jashcroft123 <54602676+jashcroft123@users.noreply.github.com>
2020-07-07 14:26:41 +0100

    Add files via upload

---
+++ CurtainStepper.ino
@@ -0,0 +1,78 @@
+#include <Stepper.h>
+const int stepsPerRevolution = 205;
+const int enablePin = D1;
+Stepper myStepper(stepsPerRevolution, D3, D4, D5, D6);
+int stepsPerTotal = stepsPerRevolution *11;
+double counter = 0; //number of steps moved /10
+float numOfSteps = 0;
+int percentClosed = 0;
+
+#include <sunset.h>
+SunSet sun;
+#define LATITUDE        52.898060
+#define LONGITUDE       -1.308730
+#define CST_OFFSET      0
+#define DST_OFFSET      1
+
+
+String sun2Console[2]; //0 sunrise, 1 sunset
+bool triggered[2] = {true, true};
+
+int changeStateCloud(String command);
+int cloudRequest(String percentClosedRequest);
+
+double sunriseTime;
+double sunsetTime;
+
+#define buttonCurtainPin  D0 // push button to open and close curtains
+int buttonCurtain = 0;
+
+//define states
+#define open 1
+#define closed 2
+#define moving 3
+
+int curtainState = open; //curtains set to open for intialisation
+int oldCurtainState = open;
+
+bool printBit;
+hello
+
+unsigned long currentTime;
+unsigned long previousTime;
+unsigned long lastDebounceTime;
+
+
+void setup() {
+  Serial.begin(9600);
+
+  myStepper.setSpeed(60); //initialise Stepper
+
+  RGB.brightness(0); // all of the way off (MAX DARK)
+
+  pinMode(buttonCurtainPin, INPUT_PULLDOWN); //button to close curtain
+  pinMode(enablePin, OUTPUT); //button to close curtain
+
+
+  sun.setPosition(LATITUDE, LONGITUDE, DST_OFFSET);
+  Particle.variable("Sunrise (HH:MM:SS)", sun2Console[0]);
+  Particle.variable("Sunset  (HH:MM:SS)", sun2Console[1]);
+
+
+  Particle.variable("State", curtainState);
+  Particle.variable("Percent Closed", percentClosed);
+
+  Particle.function("State Set", changeStateCloud);
+  Particle.function("Move", cloudRequest);
+
+  waitUntil(Particle.syncTimeDone);
+  delay(500);
+  sunriseTimeCheck();
+  sunsetTimeCheck();
+}
+
+void loop() {
+  requestCheck();
+  timeUpdate();
+  state();
+}
---
+++ Function.ino
@@ -0,0 +1,263 @@
+void timeUpdate() {
+  currentTime = millis();
+  const int printStateInterval = 10 * 60 * 1000; //report state every 10 minutes
+
+  if (currentTime - previousTime > printStateInterval) {
+    printBit = true; //print state
+    previousTime = currentTime;
+  }
+}
+
+void state() {
+  //function check the state of curtains and returns via console
+  switch (curtainState) {
+    case open:
+      openState();
+      break;
+
+    case closed:
+      closedState();
+      break;
+
+    case moving:
+      operateMotor();
+      break;
+
+    default:
+      error();
+      break;
+  }
+}
+
+void openState () {
+  if (printBit == true) {
+    Particle.publish("Curtain State", String("Open"));
+    printBit = false;
+  }
+}
+
+void closedState () {
+  if (printBit == true) {
+    Particle.publish("Curtain State", String("Closed"));
+    printBit = false;
+  }
+}
+
+void error() {
+  if (printBit == true) {
+    Particle.publish("Curtain State", String("ERROR NO STATE"));
+    printBit = false;
+  }
+}
+
+void operateMotor() {
+  if (numOfSteps == 0){
+    if (oldCurtainState == closed)
+        numOfSteps = percentClosed;
+    else if (oldCurtainState == open)
+        numOfSteps = 100 - percentClosed;
+    numOfSteps=numOfSteps*stepsPerTotal/100;
+  }
+
+  digitalWrite(enablePin,HIGH);
+  if (oldCurtainState == open) {
+    myStepper.step(-1);
+  }
+  else if (oldCurtainState == closed) {
+    myStepper.step(1);
+  }
+  counter = counter + 1;
+  if (counter >= (numOfSteps)) {
+    endOfMovement();
+  }
+}
+
+void endOfMovement() {
+  digitalWrite(enablePin,LOW);
+  int percentMoved = (counter/(stepsPerRevolution*11))*100;
+
+  if (oldCurtainState == open) {
+    curtainState = closed;
+    percentClosed = percentClosed + percentMoved;
+    if (percentClosed > 100)
+        percentClosed = 100;
+  }
+  else if (oldCurtainState == closed) {
+    curtainState = open;
+    percentClosed = percentClosed - percentMoved;
+    if (percentClosed < 0)
+        percentClosed = 0;
+  }
+  oldCurtainState = moving;
+  String stateString = "Ending Movement";
+  printBit = true;
+  numOfSteps = 0;
+  counter = 0;
+}
+
+void requestCheck() {
+  buttonCurtain = digitalRead(buttonCurtainPin); //check button state
+  if (currentTime - lastDebounceTime >= 500) {
+    if (buttonCurtain == HIGH) {
+      buttonRequest();
+      lastDebounceTime = currentTime;
+    }
+  }
+  if (curtainState != moving) {
+    sunRequest();
+  }
+}
+
+void buttonRequest() {
+    //user requests state change using button
+  if (curtainState != moving) {
+      //if not moving start Moving
+    Particle.publish("Curtain State", String("Started Moving"));
+    oldCurtainState = curtainState;
+    curtainState = moving;
+  }
+  else if (curtainState == moving) {
+    //if already operating and button pressed stop
+    endOfMovement();
+  }
+}
+
+int cloudRequest(String percentClosedRequest){
+    int request = percentClosedRequest.toInt();
+    if (request >100)
+        request = 100;
+    else if (request < 0)
+        request = 0;
+
+    if(curtainState != moving){
+        if (request < percentClosed) {
+            numOfSteps = percentClosed - request;
+            curtainState = closed;
+        }
+        else if (request>percentClosed){
+            curtainState = open;
+            numOfSteps = request - percentClosed;
+        }
+        numOfSteps = numOfSteps * stepsPerTotal/100;
+
+        if (numOfSteps != 0)
+        buttonRequest();
+    }
+    else{
+        buttonRequest();
+    }
+    return request;
+}
+
+void sunRequest() {
+  Particle.syncTime();
+  int offset = Time.getDSTOffset();
+  Time.zone(offset);
+  static int first = true;
+
+
+  static int currentDay;
+  static int currentMonth = Time.month();
+  static int currentYear = Time.year();
+  double currentSunTime =  (Time.hour()) + Time.minute()/100.0+Time.second()/10000.0;
+
+
+  if (currentDay < Time.day() || currentMonth < Time.month() || currentYear<Time.year()) {
+       //check if sun rise/set to toggle open or closed
+        sunriseTimeCheck();
+        sunsetTimeCheck();
+        currentDay = Time.day();
+        currentMonth = Time.month();
+        currentYear = Time.year();
+        memset(triggered,false,2);
+  }
+
+  if (first == true){
+      //checks time compared to sunset and rise times on first start up
+      if (currentSunTime > sunriseTime)
+        triggered[0] = true;
+      if (currentSunTime > sunsetTime)
+        triggered [1] = true;
+      first = false;
+  }
+
+
+  if (currentSunTime > sunriseTime && triggered[0] == false && currentSunTime <12) {
+    //open for morining
+    triggered[0] = true;
+    if (percentClosed > 0) {
+      curtainState = closed;
+      buttonRequest();
+    }
+  }
+
+  if (currentSunTime > sunsetTime && triggered[1] == false) {
+    //close for night
+    triggered[1] = true;
+    if (percentClosed < 100) {
+      curtainState = open;
+      buttonRequest();
+    }
+  }
+}
+
+double sunriseTimeCheck(){
+    sun.setCurrentDate(Time.year(), Time.month(), Time.day());
+    double sunrise = sun.calcSunrise();
+
+    int sunriseHour = sunrise / 60;
+    int sunriseMinute = ((sunrise / 60) - sunriseHour) * 60;
+    float sunriseSeconds = ((((sunrise/60)- sunriseHour)*60)-(sunriseMinute))*60 +0.5;
+    sunriseTime = sunriseHour + sunriseMinute/100.00 + sunriseSeconds/10000;
+
+    write2Console(0,sunriseHour,sunriseMinute,sunriseSeconds);
+
+    if (sunrise < 540)
+        sunriseTime = 9;
+}
+
+double sunsetTimeCheck(){
+    double sunset = sun.calcSunset();
+    int sunsetHour = sunset / 60;
+    int sunsetMinute = (((sunset / 60) - sunsetHour) * 60);
+    float sunsetSeconds = ((((sunset/60)- sunsetHour)*60)-(sunsetMinute))*60 + 0.5;
+    sunsetTime = sunsetHour + sunsetMinute/100.0 + sunsetSeconds/10000.0;
+
+    write2Console(1,sunsetHour,sunsetMinute,sunsetSeconds);
+}
+
+void write2Console(int i, int sunHour, int sunMinute, int sunSeconds){
+    sun2Console[i] = String(sunHour);
+    sun2Console[i] += ":";
+
+    if (sunMinute <10)
+        sun2Console[i] += "0";
+    sun2Console[i] += String(sunMinute);
+
+    sun2Console[i] += ":";
+
+    if (sunSeconds < 10)
+        sun2Console[i] += "0";
+    sun2Console[i] += String(sunSeconds);
+}
+
+int changeStateCloud(String command) {
+  command.toLowerCase();
+
+  if(command == "open" || command == "1")
+  {
+    percentClosed = 0;
+    oldCurtainState = curtainState;
+    curtainState = open;
+    return open;
+  }
+
+  else if (command =="closed" || command == "2"){
+     percentClosed = 100;
+     oldCurtainState = curtainState;
+     curtainState = closed;
+     return closed;
+  }
+
+  else return -1;
+}
